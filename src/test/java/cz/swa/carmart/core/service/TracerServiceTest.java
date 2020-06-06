e59689d786ecbdfb2b416d69816a5b0121210f1a
Michal Matija <matijmic@fel.cvut.cz>
2020-06-06 23:58:14 +0200

    add docker-compose.test.yml

--- src/test/java/cz/swa/carmart/core/service/TracerServiceTest.java
+++ src/test/java/cz/swa/carmart/core/service/TracerServiceTest.java
@@ -1,6 +1,49 @@
 package cz.swa.carmart.core.service;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import cz.swa.carmart.core.entity.Tracer;
+import cz.swa.carmart.core.repository.TracerRepository;
+
+@RunWith(SpringRunner.class)
 public class TracerServiceTest {
 
-    
+    @InjectMocks
+    TracerServiceImpl tracerService;
+
+    @Mock
+    private TracerRepository tracerRepository;
+
+    @Before
+    public void setup() {
+        tracerService = new TracerServiceImpl(tracerRepository);
+    }
+
+    @Test
+    public void saveTracerTest() {
+        Tracer tracer = createTracer();
+
+        tracerService.saveTracer(tracer);
+
+        Mockito.verify(tracerRepository, Mockito.times(1)).save(Mockito.any(Tracer.class));
+    }
+
+    Tracer createTracer() {
+        Tracer tracer = new Tracer();
+
+        tracer.setModel("Audi");
+        tracer.setPriceFrom(200);
+        tracer.setPriceTo(500);
+        tracer.setUserId(2L);
+
+        return tracer;
+    }
+
+
 }
